e4ed521d4d34709eadab5db5a76a97c204122100
Reyes <mreyesparra56@gmail.com>
2026-02-19 10:35:35 -0500

    crear proyecto

--- tuki.js
+++ tuki.js
@@ -8,7 +8,22 @@
 
 
 
+const listaDePaises=["argentina", "venezuela", "vaticano"]
+const nuevosPaises = listaDePaises.push("colombia")
 
+console.log(listaDePaises)//array otiginal + mutacion
+console.log("tamaño:",nuevosPaises)//tamaño
+//pop- mutable
+const eliminarPais = listaDePaises.pop()
+console.log(eliminarPais)
+console.log(listaDePaises)
+listaDePaises[0]= "zamba"
+console.log(listaDePaises)
+const emptyArray= []
+console.log(emptyArray.pop())
+
+listaDePaises.splice(1,1)
+console.log(listaDePaises)
 
 
 
