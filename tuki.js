9cfacdd78a600772b127751997915d514a7ec50a
mreyesparra56-star <mreyesparra56@gmail.com>
2026-02-19 11:19:26 -0500

    Remove array operations and console logs from tuki.js
    Removed several operations on the listaDePaises array, including push, pop, and splice methods, along with console logs.

--- tuki.js
+++ tuki.js
@@ -8,22 +8,7 @@
 
 
 
-const listaDePaises=["argentina", "venezuela", "vaticano"]
-const nuevosPaises = listaDePaises.push("colombia")
-
-console.log(listaDePaises)//array otiginal + mutacion
-console.log("tamaño:",nuevosPaises)//tamaño
-//pop- mutable
-const eliminarPais = listaDePaises.pop()
-console.log(eliminarPais)
-console.log(listaDePaises)
-listaDePaises[0]= "zamba"
-console.log(listaDePaises)
-const emptyArray= []
-console.log(emptyArray.pop())
-
-listaDePaises.splice(1,1)
-console.log(listaDePaises)
+tuki
 
 
 
